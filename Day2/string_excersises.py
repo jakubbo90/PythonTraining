d08cb0ba4beb7997798cea4fd76150d171d9936a
jakubbo90 <jakubb_o@wp.pl>
2019-06-26 20:30:26 +0200

    code update

--- Day2/string_excersises.py
+++ Day2/string_excersises.py
@@ -2,6 +2,6 @@ imie="Jakub"
 nazwisko="Olech"
 powitanie='Witaj '+imie+" "+nazwisko
 print(powitanie)
-print(f'{Witaj, {imie}{nazwisko}')
+print(f'Witaj, {imie}{nazwisko}')
 print("Witaj",imie,nazwisko)
 print(imie.strip('b'))
